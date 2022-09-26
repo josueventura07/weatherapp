26cbe866195c31063e7a44720c86556668dcaa4d
josueventura07 <josueandresventura@gmail.com>
2022-09-26 16:26:48 -0400

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
+import Loading from './components/Loading'
 import WeatherCard from './components/WeatherCard'
 
 function App() {
 
 const [coords, setCoords] = useState()
 const [weather, setWeather] = useState()
+const [temperature, setTemperature] = useState()
+ 
 
 useEffect(() => {
   const success = pos => {
@@ -20,22 +23,32 @@ useEffect(() => {
 navigator.geolocation.getCurrentPosition(success)
 }, [])
 
-
+ 
 useEffect(() => {
   if(coords) {
   const APIKEY = '292dc31945c32f5484206d9867a3f41f'
   const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
   axios.get(URL)
-  .then(res => setWeather(res.data))
+  .then(res => { 
+    const celsius = (res.data.main.temp - 273.15).toFixed(0)
+    const fahrenheit =  (celsius * 9/5 + 32).toFixed(0)
+    setTemperature({celsius, fahrenheit}) 
+    setWeather(res.data)
+  })
   .catch(err => console.log(err))
   }
 }, [coords])
 
-  console.log(weather);
+  
 
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      {
+        weather ? 
+          <WeatherCard weather={weather} temperature={temperature}/>
+        :
+          <Loading />  
+    }
     </div>
   )
 }
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
-const WeatherCard = ({weather}) => {
+const WeatherCard = ({weather, temperature}) => {
+
+  const [isCelsius, setIsCelsius] = useState(true)
+
+  const changeTemperature = () => setIsCelsius(!isCelsius)
+
+  console.log(weather);
+
   return (
-    <article>
-        <h1>Weather App</h1>
-
+    <article className='card'>
+        <h1 className='card_title'>Weather App</h1>
+        <h2 className='card_subtitle'>{`${weather?.name}, ${weather?.sys.country}`}</h2>
+        <section className='card_first-content'>
+          <img className='card_icon' src={weather ? `https:openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png` : ''} alt="img" />
+          
+        </section>
+        <section className='card_second-content'>
+          <h3>{weather?.weather[0].description}</h3>
+          <ul className='info_content'>
+            <li className='info_content-list'><span className='info_title'>Wind Speed:</span> {weather?.wind.speed} m/s</li>
+            <li className='info_content-list'><span className='info_title'>Clouds:</span> {weather?.clouds.all} %</li>
+            <li className='info_content-list'><span className='info_title'>Pressure:</span> {weather?.main.pressure} hPa</li>
+          </ul>
+        </section>
+        <h2 className='temperature'>{isCelsius ? `${temperature?.celsius} °C` : `${temperature?.fahrenheit} °F`}</h2>
+        <button className='card_btn'onClick={changeTemperature}>{isCelsius ? 'Change to °F' : 'Change to °C'}</button>
     </article>
   )
 }
